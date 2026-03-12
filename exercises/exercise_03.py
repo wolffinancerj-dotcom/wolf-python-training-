a5a874b8ac4603f45de5829658db305be03f27ac
Matheus Carvalho <61256001+matheuspc3@users.noreply.github.com>
2026-03-12 14:11:26 -0300

    Add comment with guide link for daily returns calculation
    Added a comment with a link to a practical guide for calculating daily returns.

--- exercises/exercise_03.py
+++ exercises/exercise_03.py
@@ -1,1 +1,2 @@
 #  Dado o csv petr4.csv com preços diários de um ativo na coluna 'price', calcule o retorno percentual diário.
+#  https://qodalabs.com/calcular-retornos-de-acoes-com-python-guia-pratico/
