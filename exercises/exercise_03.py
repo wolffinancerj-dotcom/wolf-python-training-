00eb6307acd3f86257912ed5962d90618be940ca
Matheus Carvalho <61256001+matheuspc3@users.noreply.github.com>
2026-03-12 14:31:54 -0300

    Update comment for clarity in exercise_03.py

--- exercises/exercise_03.py
+++ exercises/exercise_03.py
@@ -1,4 +1,4 @@
-#  Dado o csv petr4.csv com preços diários de um ativo na coluna 'price', calcule o retorno percentual diário.
+#  Dado o arquivo petr4.csv, com preços diários de um ativo na coluna 'price', calcule o retorno percentual diário.
 
 #  https://medium.com/turing-talks/an%C3%A1lise-de-uma-a%C3%A7%C3%A3o-em-python-b8114bce2fc
 #  https://qodalabs.com/calcular-retornos-de-acoes-com-python-guia-pratico/
