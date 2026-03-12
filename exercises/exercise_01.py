8db0a972e10e47ff8f4d598498e81240a0ad319c
Matheus Carvalho <61256001+matheuspc3@users.noreply.github.com>
2026-03-12 14:22:43 -0300

    Refactor comments for clarity in exercise_01.py

--- exercises/exercise_01.py
+++ exercises/exercise_01.py
@@ -1,14 +1,14 @@
-# Crie 2 listas: uma com 5 nomes(João, Maria, Kleber, Caio e Sarah) e outra com 5
-# valores em reais(R$) correspondentes ao saldo da conta do usuário(1350,20; 240,50;
-# 30,00; 830,15 e 50,00), e usando laços de repetição imprima os dados da seguinte
-# forma(o preenchimento das listas deve ser feito também com laços de repetição do
-# mesmo modo que será impresso: salvar nome e depois salvar o saldo
-# correspondente):
+# Crie 2 listas: uma com 5 nomes (João, Maria, Kleber, Caio e Sarah) e outra com 5 valores em reais(R$) correspondentes ao saldo da conta do usuário(1350,20; 240,50; 30,00; 830,15 e 50,00), 
+# e usando laços de repetição imprima os dados da seguinte forma(o preenchimento das listas deve ser feito também com laços de repetição do mesmo modo que será impresso: salvar nome e depois salvar o saldo correspondente):
+
 # Entradas:
-# Insira o nome: ****
-# Insira o saldo: ****
-# ….
+
+# Insira o nome: 
+# Insira o saldo: 
+
+
 # Saída/Impressão:
+
 # LISTA DE CLIENTES - BANCO NACIONAL
 # NOME SALDO CONTA
 # nome0 saldo0 #0
